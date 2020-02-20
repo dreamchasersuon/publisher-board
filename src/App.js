97f7ac3857a045b89783bde3a6dfe9317fcfb6fc
Tim <formulatim@gmail.com>
2020-02-20 20:12:13 +0300

    add margin bottom to header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './components/header/Header';
+import Table from './components/table/Table';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <Table />
+      <button className="App-button">LOAD MORE</button>
     </div>
   );
 }
