8864a81ada0b4cad221800b98fd252e634363ffe
Tim <formulatim@gmail.com>
2020-02-25 16:56:34 +0300

    add header with tests

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '../buttons/button-open-modal/Button';
 import './Header.css';
 
-export default function Header() {
+export default function Header({ handleOnOpenModal, title, caption }) {
   return (
     <header className="Header">
       <div className="Header-title">
-        <h1>Players</h1>
-        <span>manage users of your game</span>
+        <h1>{title}</h1>
+        <span>{caption}</span>
       </div>
-      <button className="Header-button">Add</button>
+      <Button
+        onPress={handleOnOpenModal}
+        title="Add"
+        dataName="addUser"
+        className="Header-button"
+      />
     </header>
   );
 }
+
+Header.propTypes = {
+  handleOnOpenModal: PropTypes.func,
+  title: PropTypes.string,
+  caption: PropTypes.string,
+};
--- src/components/header/Header.test.js
+++ src/components/header/Header.test.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import Header from './Header';
 
-test('renders header', () => {
-  const { getByText } = render(<Header />);
-  const headerTitle = getByText(/Players/i);
-  const headerCaption = getByText(/manage users of your game/i);
-  const button = getByText(/Add/i);
-
-  expect(headerTitle).toBeInTheDocument();
-  expect(headerCaption).toBeInTheDocument();
-  expect(button).toBeInTheDocument();
+test('renders header with title and caption', () => {
+  const tree = renderer
+    .create(<Header title="Players" caption="manage users of your game" />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
