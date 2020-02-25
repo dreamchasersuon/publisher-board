5c7a95460c926b1ff44558a35d8fcae62c21398e
Tim <formulatim@gmail.com>
2020-02-25 16:54:38 +0300

    add root error boundary

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({ error, errorInfo });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>{String(this.state.error)}</h1>;
+    }
+
+    return this.props.children;
+  }
+}
